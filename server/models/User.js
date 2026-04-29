5d2590e74ec38346d70050dca54be401443a7f00
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-04-29 16:25:55 +0530

    add timestamps

--- server/models/User.js
+++ server/models/User.js
@@ -14,6 +14,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+},
+{
+    timestamps: true
 });
 
 module.exports = mongoose.model("User", userSchema);
