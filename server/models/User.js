ce24c65095cd27b47657203dad7006b21d566697
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-04-29 22:34:45 +0530

    develop note schema model

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
     },
 },
 {
