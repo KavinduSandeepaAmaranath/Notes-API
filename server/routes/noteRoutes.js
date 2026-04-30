af9333a0ed2042b58f5d5e0082f2925d72b0d16b
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-04-30 22:00:21 +0530

    modify noteRoutes

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const Note = require("../models/Note");
 const auth = require("../middleware/auth");
 
-router.post("/notes", auth, async(requestAnimationFrame, res, next) => {
+router.post("/notes", auth, async (req, res, next) => {
     try {
         const { title, content } = req.body;
 
         const note = new Note({
             title,
             content,
-            userId: req.userId,    
+            userId: req.user.id,    
         });
 
         await note.save();
@@ -19,6 +19,18 @@ router.post("/notes", auth, async(requestAnimationFrame, res, next) => {
 
     } catch (error) {
         res.status(500).json({ message: "Error Creating Note" });
+    }
+});
+
+router.get("/notes", auth, async (req, res) => {
+    try {
+        const notes = await Note.find({
+            userId: req.user.id,
+        });
+
+        res.json(notes);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching notes" });
     }
 });
 
